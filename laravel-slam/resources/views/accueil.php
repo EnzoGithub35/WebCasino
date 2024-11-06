7b2bcc84e6b2ed3b7af7a5b5fdcff15cf28ccebb
EnzoGithub35 <enzo.reine@ecoles-epsi.net>
2024-11-06 16:28:26 +0100

    t

--- laravel-slam/resources/views/accueil.php
+++ laravel-slam/resources/views/accueil.php
@@ -3,6 +3,7 @@
 <header>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <title>Document</title>
     <style>
 
