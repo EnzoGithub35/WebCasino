fcfe9b010df285b4ac9d367333e7dd27e5e50689
EnzoGithub35 <enzo.reine@ecoles-epsi.net>
2024-11-12 13:41:51 +0100

    Fix asset helper syntax

--- laravel-slam/resources/views/index.blade.php
+++ laravel-slam/resources/views/index.blade.php
@@ -9,6 +9,7 @@
 <body class="body">
     <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js" integrity="sha512-VEd+nq25CkR676O+pLBnDW09R7VQX9Mdiij052gVCp5yVH3jGtH70Ho/UUv4mJDsEdTvqRCFZg0NKGiojGnUCw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="{{ asset('js/script.js') }}"></script>
 
     @foreach (['deconnect_message'] as $msg)
         @if (\Session::has($msg))
