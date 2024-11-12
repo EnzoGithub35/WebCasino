a1a1fbf5d2bf2321075c4f33e9dcc2d33495b6ed
EnzoGithub35 <enzo.reine@ecoles-epsi.net>
2024-11-12 12:10:19 +0100

    Update CSS references and compile assets

--- laravel-slam/resources/views/index.blade.php
+++ laravel-slam/resources/views/index.blade.php
@@ -1,47 +1,25 @@
-<!-- resources/views/index.blade.php -->
-
-@if (session_status() == PHP_SESSION_NONE)
-    @php
-        session_start();
-    @endphp
-@endif
-
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <title>Accueil</title>
-        <link rel="stylesheet" href="{{ 'style.css' }}"> 
-        <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-        <link rel="stylesheet" href="{{ asset('style.css') }}">
-        <link rel="stylesheet" href="css/style.css">
-        <link rel="stylesheet" href="{{ 'css/style.css' }}"> 
-        <link rel="stylesheet" href="css/style.css">
-        
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.css" integrity="sha512-3pIirOrwegjM6erE5gPSwkUzO+3cTjpnV9lexlNZqvupR64iZBnOOTiiLPb9M36zpMScbmUNIcHUqKD47M719g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+<head>
+    <meta charset="UTF-8">
+    <title>Accueil</title>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.css" integrity="sha512-3pIirOrwegjM6erE5gPSwkUzO+3cTjpnV9lexlNZqvupR64iZBnOOTiiLPb9M36zpMScbmUNIcHUqKD47M719g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
 <body class="body">
-<script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js" integrity="sha512-VEd+nq25CkR676O+pLBnDW09R7VQX9Mdiij052gVCp5yVH3jGtH70Ho/UUv4mJDsEdTvqRCFZg0NKGiojGnUCw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js" integrity="sha512-VEd+nq25CkR676O+pLBnDW09R7VQX9Mdiij052gVCp5yVH3jGtH70Ho/UUv4mJDsEdTvqRCFZg0NKGiojGnUCw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
+    @foreach (['deconnect_message'] as $msg)
+        @if (\Session::has($msg))
+            <script>
+                toastr.success("{{ \Session::get($msg) }}");
+            </script>
+        @endif
+    @endforeach
 
-@foreach (['deconnect_message'] as $msg)
-    @if (\Session::has($msg))
-        <script>
-            toastr.success("{{ \Session::get($msg) }}");
-        </script>
-    @endif
-@endforeach
     @include('navbar')
     @include('accueil')
-
-
-
-    
-
-
 </body>
-
-
-
 @include('footer')
 </html>
